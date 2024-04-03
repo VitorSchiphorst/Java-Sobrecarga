e6a26b764bb714703efd39963af9d3a42adbdf9a
Vitor Hugo Schiphorst <vitorhugo.schiphorst@gmail.com>
2024-04-02 21:04:12 -0300

    Atualizando 3.0

--- Animais/src/Oviparo.java
+++ Animais/src/Oviparo.java
@@ -1,4 +1,24 @@
 public class Oviparo extends Animal {
-    private Integer qtdOvos;
-    private Integer qtdEmbrioes;
+    private Integer qtdReproducao;
+
+    public Integer getQtdReproducao() {
+        return qtdReproducao;
+    }
+
+    public void setQtdReproducao(Integer qtdReproducao) {
+        this.qtdReproducao = qtdReproducao;
+    }
+
+    public Oviparo() {
+
+    }
+
+    public Oviparo(String nome, Integer qtdPatas) {
+        super(nome, qtdPatas);
+    }
+
+    public Oviparo(String nome, Integer qtdPatas, Integer qtdReproducao) {
+        super(nome, qtdPatas);
+        this.setQtdReproducao(qtdReproducao);
+    }
 }
--- Animais/src/Reptil.java
+++ Animais/src/Reptil.java
@@ -1,3 +1,7 @@
 public class Reptil extends Oviparo {
 
+    public Reptil (String nome, Integer qtdPatas, Integer qtdReproducao) {
+        super(nome, qtdPatas, qtdReproducao);
+    }
+
 }
